8bcb2706574690a3a05e68c34128e9de1418f33b
Armağan Gök <1armagangok@gmail.com>
2022-01-27 15:35:42 +0300

    new structure for building best practise

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'app/views/home/homeview.dart';
+import 'screens/screen_home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Template App',
-      home: HomeView(),
+      home: HomeScreen(),
     );
   }
 }
